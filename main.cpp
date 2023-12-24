f86244ac853e6683a8b52868b8a135d308f8926f
dozerdozerdozer <142627025+dozerdozerdozer@users.noreply.github.com>
2023-12-24 21:28:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 void voyager() {
     for (int N = 2; N <= 10; ++N) {
         int exps_count = 10;
-        double ttl_quality = 0;
+        double ttl_quality_for_X = 0;
+        double ttl_quality_for_G = 0;
 
         for (int exp = 0; exp < exps_count; ++exp) {
 
@@ -22,17 +23,22 @@ void voyager() {
             std::vector<int> points_arr = TSP::points_arr(N);
             double optimal_length = TSP::opt_route_lenght(points, points_arr);
 
-            ttl_quality += TSP::approx_quality(graph, points) / optimal_length;
+            auto C = TSP::approx_quality(graph, points);
+            ttl_quality_for_X += std::max(C / optimal_length, optimal_length / C);      // ro(n)
+            ttl_quality_for_G += (C - ttl_quality_for_X*ttl_quality_for_X) / optimal_length;
         }
 
-        double avg_quality = ttl_quality / exps_count;
-        std::cout << "N = " << N << ", avg quality = " << avg_quality << std::endl;
+        double avg_quality_X = ttl_quality_for_X / exps_count;
+        double avg_quality_G = ttl_quality_for_G / exps_count;
+        std::cout << "N = " << N << ", среднее значение качества приближения = " << avg_quality_X << ", среднеквадратичное отклонение качества приближения = " << avg_quality_G <<std::endl;
     }
 }
-
 
 
 int main() {
     voyager();
     return 0;
 }
+
+
+
